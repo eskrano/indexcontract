d811b386b4eb271e4a2431a799c9f644cd26d586
Alex Priadko <mr.eskrano@gmail.com>
2021-03-28 14:17:56 +0300

    Fist stage and end haha

--- contracts/IndexToken.sol
+++ contracts/IndexToken.sol
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -28,18 +29,18 @@ contract IndexToken is ERC20, ERC20Burnable, Ownable {
     constructor(
         address token0_addr,
         address token1_addr,
-        address token2_addr,
-        address token0_uni_addr,
-        address token1_uni_addr,
-        address token2_uni_addr
+        address token2_addr
+        // address token0_uni_addr,
+        // address token1_uni_addr,
+        // address token2_uni_addr
     ) ERC20("INDEX", "INDEX") {
         token0 = token0_addr;
         token1 = token1_addr;
         token2 = token2_addr;
 
-        token0_uni = token0_uni_addr;
-        token1_uni = token1_uni_addr;
-        token2_uni = token2_uni_addr;
+        // token0_uni = token0_uni_addr;
+        // token1_uni = token1_uni_addr;
+        // token2_uni = token2_uni_addr;
     }
 
     function buyIndex(uint256 tokens_amount) public {
@@ -48,6 +49,7 @@ contract IndexToken is ERC20, ERC20Burnable, Ownable {
         require(IERC20(token0).balanceOf(msg.sender) >=  token0_amount, "Token0 is low balance");
         require(IERC20(token1).balanceOf(msg.sender) >=  token1_amount, "Token1 is low balance");
 
+    
         /** transfer tokens **/
         require(IERC20(token0).transferFrom(msg.sender, address(this), token0_amount), "Failed to transfer token0");
         require(IERC20(token1).transferFrom(msg.sender, address(this), token1_amount), "Failed to transfer token1");
@@ -60,16 +62,29 @@ contract IndexToken is ERC20, ERC20Burnable, Ownable {
     function sellIndex(uint256 tokens_amount) public {
 
         require(balanceOf(msg.sender) >= tokens_amount, "Invalid index amount");
+        require(allowance(msg.sender, address(this)) >= tokens_amount, "Invalid allowance. Please approve your tokens.");
 
         uint256 token0_amount = tokens_amount.div(3).mul(2);
         uint256 token1_amount = tokens_amount.div(3);
 
         /** transfer tokens **/
-        require(IERC20(token0).transferFrom(address(this), msg.sender, token0_amount), "Failed to transfer token0");
-        require(IERC20(token1).transferFrom(address(this), msg.sender, token1_amount), "Failed to transfer token1");
+        require(IERC20(token0).transfer(msg.sender, token0_amount), "Failed to transfer token0");
+        require(IERC20(token1).transfer(msg.sender, token1_amount), "Failed to transfer token1");
 
         emit IndexSell(msg.sender, token0_amount, token1_amount, tokens_amount);
 
         _burn(msg.sender, tokens_amount);
+    }
+
+    function getToken0() public view returns (address) {
+        return token0;
+    }
+
+    function getToken1() public view returns (address) {
+        return token1;
+    }
+
+    function getToken2() public view returns (address) {
+        return token2;
     }
 }
--- test/main-test.js
+++ test/main-test.js
@@ -1,64 +1,114 @@
 const { expect } = require("chai");
 
-describe("Main Test", function() {
-  it("Main Test", async function() {
+describe("Main Test", function () {
+
+
+  before(async function () {
     let accounts = await hre.ethers.getSigners();
-    
+
     let acc0 = accounts[0];
-
-    console.log(acc0.address);
-
-    console.log(hre.ethers.utils.BigNumber.from(await hre.ethers.provider.getBalance(acc0.address)));
-
     const wBTC_factory = await hre.ethers.getContractFactory("wBTC");
     const wBTC = await wBTC_factory.deploy();
-  
+
     await wBTC.deployed();
-  
-    console.log("wBTC deployed to:", wBTC.address);
-  
+
+    //console.log("wBTC deployed to:", wBTC.address);
+
     // wETH
-  
-    
+
+
     const wETH_factory = await hre.ethers.getContractFactory("wETH");
     const wETH = await wETH_factory.deploy();
-  
+
     await wETH.deployed();
-    
-    console.log("wETH deployed to:", wETH.address);
-  
+
+    //console.log("wETH deployed to:", wETH.address);
+
     //w USD
-  
+
     const wUSD_factory = await hre.ethers.getContractFactory("wUSD");
     const wUSD = await wUSD_factory.deploy();
-  
+
     await wUSD.deployed();
-    
-    console.log("wUSD deployed to:", wUSD.address);
-  
-    console.log("Deploying INDEX contract");
-  
-    console.log("Deploying INDEX ERC-20 token");
-  
+
+    //console.log("wUSD deployed to:", wUSD.address);
+
+    //console.log("Deploying INDEX contract");
+
+    //console.log("Deploying INDEX ERC-20 token");
+
     const index_token_factory = await hre.ethers.getContractFactory("IndexToken");
-    const index_token = await index_token_factory.deploy();
-  
-    await index_token.deployed();
-  
-    console.log("Index token deployed to:", index_token.address);
-    
-  
-    const index_factory = await hre.ethers.getContractFactory('Index');
-    const index = await index_factory.deploy(wBTC.address, wETH.address, index_token.address);
-  
+    const index = await index_token_factory.deploy(wBTC.address, wETH.address, wUSD.address);
+
     await index.deployed();
-  
-    console.log("Index ROUTER deployed to:", index.address);
 
-    expect(await index.getToken0()).to.equal(wBTC.address);
-    expect(await index.getToken1()).to.equal(wETH.address);
-    expect(await index.getIndexToken()).to.equal(index_token.address);
+    this.index = index;
+    this.wETH = wETH;
+    this.wBTC = wBTC;
+    this.wUSD = wUSD;
 
+    //console.log("Index token deployed to:", index_token.address);
+  });
+
+  it("Is token0-token2 address equals to initialized?", async function () {
+    expect(await this.index.getToken0()).to.equal(this.wBTC.address);
+    expect(await this.index.getToken1()).to.equal(this.wETH.address);
+    expect(await this.index.getToken2()).to.equal(this.wUSD.address);
+  });
+
+  it("Check tokens balance for acc0", async function () {
+    let accounts = await hre.ethers.getSigners();
+    let acc_addr0 = accounts[0].address;
+    let balance_acc0_wbtc = await this.wBTC.balanceOf(acc_addr0);
+    let balance_acc0_weth = await this.wETH.balanceOf(acc_addr0);
+    let balance_acc0_wusd = await this.wUSD.balanceOf(acc_addr0);
+    let wbtc_total_supply = await this.wBTC.totalSupply();
+    let weth_total_supply = await this.wETH.totalSupply();
+    let wusd_total_supply = await this.wUSD.totalSupply();
+
+    expect(balance_acc0_wbtc.toString()).is.equals(wbtc_total_supply.toString());
+    expect(balance_acc0_weth.toString()).is.equals(weth_total_supply.toString());
+    expect(balance_acc0_wusd.toString()).is.equals(wusd_total_supply.toString());
+  });
+
+  it('Test buy index method', async function () {
+    let accounts = await hre.ethers.getSigners();
+    let acc_addr0 = accounts[0].address;
+
+    await this.wBTC.approve(this.index.address, hre.ethers.utils.parseEther('8'));
+    await this.wETH.approve(this.index.address, hre.ethers.utils.parseEther('5'));
+
+
+    let buy_void_result = await this.index.buyIndex(hre.ethers.utils.parseEther('10'));
+
+    let index_in_acc0_bal = await this.index.balanceOf(acc_addr0);
+    expect(index_in_acc0_bal.toString()).is.equals(hre.ethers.utils.parseEther('10').toString());
+  });
+
+  it('Test sell index method', async function () {
+    let accounts = await hre.ethers.getSigners();
+    let acc_addr0 = accounts[0].address;
+    let index_in_acc0_bal = await this.index.balanceOf(acc_addr0);
+
+    expect(index_in_acc0_bal.toString()).is.equals(hre.ethers.utils.parseEther('10').toString()); // check balance enough before latest test
+
+    let approve_index_to_contract = await this.index.approve(this.index.address,index_in_acc0_bal);
+    
+    let allowance_check = await this.index.allowance(acc_addr0, this.index.address);
+    expect(allowance_check.toString()).is.equals(hre.ethers.utils.parseEther('10').toString()); // check balance enough before latest test
+    //console.log("ALLOW: "+allowance_check.toString());
+
+    let sell_result = await this.index.sellIndex(hre.ethers.utils.parseEther('10'));
+    let updated_balance_index = await this.index.balanceOf(acc_addr0);
+    expect(updated_balance_index.toString()).is.equals(hre.ethers.utils.parseEther('0').toString());
+    let wbtc_balance_acc0 = await this.wBTC.balanceOf(acc_addr0);
+    let wbtc_total_supply = await this.wBTC.totalSupply();
+
+    let weth_balance_acc0 = await this.wETH.balanceOf(acc_addr0);
+    let weth_total_supply = await this.wETH.totalSupply();
+
+    expect(wbtc_balance_acc0.toString()).is.equals(wbtc_total_supply.toString());
+    expect(weth_balance_acc0.toString()).is.equals(weth_total_supply.toString());
 
   });
 });
